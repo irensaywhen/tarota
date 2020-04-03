d56828ac8339fe43d4517c39b37c927bff3550df
Irina <irensaywhen@gmail.com>
2020-04-03 15:46:51 +0300

    Register page v1

--- js/validation.js
+++ js/validation.js
@@ -2,6 +2,8 @@
 
     $(document).ready(function(){
         $('#describe-dream-form').validate();
+        $('#account_forms_log-in').validate();
+        $('#account_forms_sign-up').validate();
     })
     
 })(jQuery);
