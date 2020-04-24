775fa826fe498ba22fa495614e44d98d4f02c50b
Irina <irensaywhen@gmail.com>
2020-04-24 09:43:34 +0300

    custom.js file cleaned

--- js/custom.js
+++ js/custom.js
@@ -3,11 +3,6 @@
 
     var tpj = jQuery;
     var revapi24;
-    // Preloader 
-    jQuery(window).on('load', function() {
-        jQuery("#status").fadeOut();
-        jQuery("#preloader").delay(350).fadeOut("slow");
-    });
 
 
     // on ready function
@@ -41,61 +36,6 @@
         });
 
 
-        // Main Slider Animation
-
-        (function($) {
-
-            //Function to animate slider captions 
-            function doAnimations(elems) {
-                //Cache the animationend event in a variable
-                var animEndEv = 'webkitAnimationEnd animationend';
-
-                elems.each(function() {
-                    var $this = $(this),
-                        $animationType = $this.data('animation');
-                    $this.addClass($animationType).one(animEndEv, function() {
-                        $this.removeClass($animationType);
-                    });
-                });
-            }
-
-            //Variables on page load 
-            var $myCarousel = $('#carousel-example-generic'),
-                $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
-
-            //Initialize carousel 
-            $myCarousel.carousel();
-
-            //Animate captions in first slide on page load 
-            doAnimations($firstAnimatingElems);
-
-            //Pause carousel  
-            $myCarousel.carousel('pause');
-
-
-            //Other slides to be animated on carousel slide event 
-            $myCarousel.on('click slide.bs.carousel', function(e) {
-                var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-                doAnimations($animatingElems);
-            });
-
-
-        })(jQuery);
-
-
-        // Magnific popup-video
-        $('.popup-youtube').magnificPopup({
-            disableOn: 700,
-            type: 'iframe',
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: false,
-
-            fixedContentPos: false
-        });
-
-
-
         // -------------------------------------------------------------
         // Shuffle
         // -------------------------------------------------------------
@@ -124,178 +64,11 @@
 
                     // reshuffle grid
                     $grid.shuffle('shuffle', groupName);
+
+
                 });
             }
         });
-
-
-        //-------------------------------------------------------
-        // counter-section
-        //-------------------------------------------------------
-        $('.hs_counter_main_wrapper').on('inview', function(event, visible, visiblePartX, visiblePartY) {
-            if (visible) {
-                $(this).find('.timer').each(function() {
-                    var $this = $(this);
-                    $({
-                        Counter: 0
-                    }).animate({
-                        Counter: $this.text()
-                    }, {
-                        duration: 2000,
-                        easing: 'swing',
-                        step: function() {
-                            $this.text(Math.ceil(this.Counter));
-                        }
-                    });
-                });
-                $(this).off('inview');
-            }
-        });
-
-        $(document).ready(function() {
-            $('.hs_news_slider_wrapper .owl-carousel').owlCarousel({
-                loop: true,
-                margin: 10,
-                autoplay: true,
-                responsiveClass: true,
-                smartSpeed: 1200,
-                navText: ['<i class="flaticon-left-arrow" aria-hidden="true"></i>', '<i class="flaticon-right-arrow" aria-hidden="true"></i>'],
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: true
-                    },
-                    600: {
-                        items: 1,
-                        nav: true
-                    },
-                    1000: {
-                        items: 1,
-                        nav: true,
-                        loop: true,
-                        margin: 20
-                    }
-                }
-            })
-        })
-
-
-        $(document).ready(function() {
-            $('.hs_testi_slider_wrapper .owl-carousel').owlCarousel({
-                loop: true,
-                margin: 10,
-                autoplay: true,
-                responsiveClass: true,
-                smartSpeed: 1200,
-                navText: ['<i class="flaticon-left-arrow" aria-hidden="true"></i>', '<i class="flaticon-right-arrow" aria-hidden="true"></i>'],
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: true
-                    },
-                    600: {
-                        items: 1,
-                        nav: true
-                    },
-                    1000: {
-                        items: 1,
-                        nav: true,
-                        loop: true,
-                        margin: 20
-                    }
-                }
-            })
-        })
-
-
-        $(document).ready(function() {
-            $('.hs_client_slider_wrapper .owl-carousel').owlCarousel({
-                loop: true,
-                margin: 10,
-                autoplay: true,
-                responsiveClass: true,
-                smartSpeed: 1200,
-                navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: true
-                    },
-                    600: {
-                        items: 3,
-                        nav: true
-                    },
-                    1000: {
-                        items: 4,
-                        nav: true,
-                        loop: true,
-                        margin: 20
-                    }
-                }
-            })
-        })
-
-
-
-        $(document).ready(function() {
-            $('.hs_online_slider_wrapper .owl-carousel').owlCarousel({
-                loop: true,
-                margin: 10,
-                autoplay: true,
-                responsiveClass: true,
-                smartSpeed: 1200,
-                navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-                responsive: {
-                    0: {
-                        items: 2,
-                        nav: true
-                    },
-                    400: {
-                        items: 3,
-                        nav: true
-                    },
-                    600: {
-                        items: 4,
-                        nav: true
-                    },
-                    1000: {
-                        items: 4,
-                        nav: true,
-                        loop: true,
-                        margin: 20
-                    }
-                }
-            })
-        })
-
-
-
-        $(document).ready(function() {
-            $('.hs_rs_four_slider_wrapper .owl-carousel').owlCarousel({
-                loop: true,
-                margin: 10,
-                autoplay: true,
-                responsiveClass: true,
-                smartSpeed: 1200,
-                navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: true
-                    },
-                    600: {
-                        items: 1,
-                        nav: true
-                    },
-                    1000: {
-                        items: 1,
-                        nav: true,
-                        loop: true,
-                        margin: 20
-                    }
-                }
-            })
-        })
 
 
         $(document).ready(function() {
@@ -306,35 +79,6 @@
                 responsiveClass: true,
                 smartSpeed: 1200,
                 navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: true
-                    },
-                    600: {
-                        items: 1,
-                        nav: true
-                    },
-                    1000: {
-                        items: 1,
-                        nav: true,
-                        loop: true,
-                        margin: 20
-                    }
-                }
-            })
-        })
-
-
-
-        $(document).ready(function() {
-            $('.cc_ps_top_slider_section .owl-carousel').owlCarousel({
-                loop: true,
-                margin: 10,
-                autoplay: false,
-                responsiveClass: true,
-                smartSpeed: 1200,
-                navText: ['<i class="fa fa-angle-double-left" aria-hidden="true"></i>', '<i class="fa fa-angle-double-right" aria-hidden="true"></i>'],
                 responsive: {
                     0: {
                         items: 1,
