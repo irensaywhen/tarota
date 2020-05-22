ed28e0a654b379b248c4d919bb7fddeb36674e24
Irina <irensaywhen@gmail.com>
2020-05-22 18:38:36 +0300

    call to action is visible

--- js/scroll.js
+++ js/scroll.js
@@ -1,40 +1,36 @@
-(function($){
+(function ($) {
+  $(document).ready(function () {
+    // ===== Scroll to Top ====
+    $(window).scroll(function () {
+      if ($(this).scrollTop() >= 100) {
+        $("#return-to-top").fadeIn(200);
+      } else {
+        $("#return-to-top").fadeOut(200);
+      }
+    });
 
-    $(document).ready(function(){
+    $("#return-to-top").on("click", function () {
+      $("body, html").animate(
+        {
+          scrollTop: 0,
+        },
+        500
+      );
+    });
 
-        // ===== Scroll to Top ==== 
-        $(window).scroll(function() {
+    // Menu js for Position fixed
+    $(window).scroll(function () {
+      var window_top = $(window).scrollTop() + 1;
 
-            if ($(this).scrollTop() >= 100) {
-
-                $('#return-to-top').fadeIn(200);
-
-            } else {
-
-                $('#return-to-top').fadeOut(200);
-            }
-        });
-
-        $('#return-to-top').on('click', function() {
-
-            $('body, html').animate({
-                scrollTop: 0
-            }, 500);
-        });
-
-
-        // Menu js for Position fixed
-        $(window).scroll(function() {
-
-            var window_top = $(window).scrollTop() + 1;
-
-            if (window_top > 160) {
-
-                $('.header-navigation-wrapper').addClass('menu_fixed animated fadeInDown');
-                
-            } else {
-                $('.header-navigation-wrapper').removeClass('menu_fixed animated fadeInDown');
-            }
-        });
-    })
-})(jQuery)
+      if (window_top > 160) {
+        $(".header-navigation-wrapper").addClass(
+          "menu_fixed animated fadeInDown"
+        );
+      } else {
+        $(".header-navigation-wrapper").removeClass(
+          "menu_fixed animated fadeInDown"
+        );
+      }
+    });
+  });
+})(jQuery);
