e58fae223bf31f73974975236fe66989e1094ec7
Irina <irensaywhen@gmail.com>
2020-06-21 18:01:24 +0300

    Styles compressed into single stylesheet

--- MVPs/gulpfile.js
+++ MVPs/gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require("gulp");
 const purgecss = require("gulp-purgecss");
+const cleanCSS = require("gulp-clean-css");
+const concat = require("gulp-concat");
 
-gulp.task("removeUnusedCss", function () {
+// Remove unused CSS
+gulp.task("removeUnusedCSS", function () {
   return gulp
     .src("home/**/*.css")
     .pipe(
@@ -10,4 +13,18 @@ gulp.task("removeUnusedCss", function () {
       })
     )
     .pipe(gulp.dest("home/dist/"));
+});
+
+// Minify and concatenate CSS
+gulp.task("minifyCSS", function () {
+  return gulp
+    .src([
+      "home/dist/css/bootstrap.css",
+      "home/dist/css/fonts.css",
+      "home/dist/fonts/fontawesome/css/all.css",
+      "home/dist/css/mvp_style.css",
+    ])
+    .pipe(cleanCSS())
+    .pipe(concat("style.min.css"))
+    .pipe(gulp.dest("home/dist/css"));
 });
