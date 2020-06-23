209d397cb475d36b80b2369f240237bcd1763ac7
Irina <irensaywhen@gmail.com>
2020-06-23 19:52:43 +0300

    Home 7 mvp examples

---
+++ MVPs/home7/src/js/scroll.js
@@ -0,0 +1,21 @@
+(function ($) {
+  $(document).ready(function () {
+    // ===== Scroll to Top ====
+    $(window).scroll(function () {
+      if ($(this).scrollTop() >= 100) {
+        $("#return-to-top").fadeIn(200);
+      } else {
+        $("#return-to-top").fadeOut(200);
+      }
+    });
+
+    $("#return-to-top").on("click", function () {
+      $("body, html").animate(
+        {
+          scrollTop: 0,
+        },
+        500
+      );
+    });
+  });
+})(jQuery);
