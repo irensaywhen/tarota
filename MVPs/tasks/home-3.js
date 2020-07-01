b287558b57b8a1a0a733949ecebe8ef0dcdc6820
Irina <irensaywhen@gmail.com>
2020-07-01 09:01:32 +0300

    Gulpfile for home3 changed

--- MVPs/tasks/home-3.js
+++ MVPs/tasks/home-3.js
@@ -5,6 +5,17 @@ const cleanCSS = require("gulp-clean-css");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const terser = require("gulp-terser");
+const del = require("del");
+
+// Clean css and js folders
+gulp.task("cleanDist-11", function () {
+  return del([
+    "home3/dist/css/**",
+    "home3/dist/js/**",
+    "!home3/dist/css",
+    "!home3/dist/js",
+  ]);
+});
 
 // Remove Unused CSS
 gulp.task("removeUnusedCSS-3", function (done) {
@@ -13,10 +24,14 @@ gulp.task("removeUnusedCSS-3", function (done) {
     .pipe(
       purgecss({
         content: ["home3/src/**/*.html"],
+        whitelist: ["error"],
       })
     )
-    .pipe(gulp.dest("home3/src/clean-css"));
-  done();
+    .pipe(gulp.dest("home3/src/clean-css/"))
+    .on("end", function () {
+      // in case of success
+      done();
+    });
 });
 
 // Minify and concatenate CSS
@@ -28,6 +43,10 @@ gulp.task("minifyCSS-3", function (done) {
       "home3/src/clean-css/fonts/fontawesome/css/all.css",
       "home3/src/clean-css/css/style.css",
     ])
+    .on("error", (error) => {
+      console.log(error);
+      done(error);
+    })
     .pipe(
       autoprefixer({
         overrideBrowserslist: ["last 99 versions"],
@@ -36,8 +55,16 @@ gulp.task("minifyCSS-3", function (done) {
     )
     .pipe(cleanCSS({ level: { 1: { specialComments: 0 } } }))
     .pipe(concat("style.min.css"))
-    .pipe(gulp.dest("home3/dist/css"));
-  done();
+    .pipe(gulp.dest("home3/dist/css"))
+    .on("end", function () {
+      // in case of success
+      done();
+    });
+});
+
+// Delete clean-css folder
+gulp.task("deleteCleanCSS-3", function () {
+  return del(["home3/src/clean-css"]);
 });
 
 // Minify and concatenate JavaScript for the index page
@@ -46,13 +73,20 @@ gulp.task("minifyJSMain-3", function (done) {
     .src([
       "home3/src/js/jquery.js",
       "node_modules/jquery-validation/dist/jquery.validate.js",
-      "home3/src/js/form.js",
       "home3/src/js/scroll.js",
+      "home3/src/js/form.js",
     ])
     .pipe(concat("main.js"))
+    .on("error", (error) => {
+      console.log(error);
+      done(error);
+    })
     .pipe(terser())
-    .pipe(gulp.dest("home3/dist/js"));
-  done();
+    .pipe(gulp.dest("home3/dist/js"))
+    .on("end", function () {
+      // in case of success
+      done();
+    });
 });
 
 // Minify and concatenate JavaScript for the payment approval page
@@ -60,7 +94,33 @@ gulp.task("minifyJSPayment-3", function (done) {
   gulp
     .src(["home3/src/js/jquery.js", "home3/src/js/scroll.js"])
     .pipe(concat("payment.js"))
+    .on("error", (error) => {
+      console.log(error);
+      done(error);
+    })
     .pipe(uglify())
-    .pipe(gulp.dest("home3/dist/js"));
-  done();
+    .pipe(gulp.dest("home11/dist/js"))
+    .on("end", function () {
+      // in case of success
+      done();
+    });
 });
+
+// Handle all the css
+gulp.task(
+  "CSS-3",
+  gulp.series("removeUnusedCSS-3", "minifyCSS-3", "deleteCleanCSS-3")
+);
+
+// Handle all the JS
+gulp.task(
+  "JS-3",
+  gulp.parallel("minifyJSImages-3", "minifyJSMain-3", "minifyJSPayment-3")
+);
+
+// Build CSS and JS files
+
+gulp.task(
+  "build-3",
+  gulp.series("cleanDist-3", gulp.parallel("CSS-3", "JS-3"))
+);
